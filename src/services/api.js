d31dd793d53e1eee417c81d4f0478e6ab4e0e6c1
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-06-02 14:47:51 +0200

    add chrome extension signer

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import {Client as ApiClient} from "@tronscan/client";
+import {ChromeExtensionSigner} from "@tronscan/client/src/signer/chromeExtensionSigner";
 import io from 'socket.io-client';
 
 export const Client = new ApiClient();
+
+Client.setSigner(new ChromeExtensionSigner());
+
 
 export function buildClient(account) {
   return new ApiClient();
