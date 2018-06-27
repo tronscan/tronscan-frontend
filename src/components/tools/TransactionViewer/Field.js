cc4d5a047f83c1eeb76d9293ee7e4f626937640c
zonLi <lizongkai@tron.network>
2018-06-27 11:59:42 +0800

    add tbody

--- src/components/tools/TransactionViewer/Field.js
+++ src/components/tools/TransactionViewer/Field.js
@@ -2,11 +2,13 @@ import {tu} from "../../../utils/i18n";
 import React from "react";
 
 
-export default function Field({ label, children }) {
+export default function Field({label, children}) {
   return (
-    <tr>
-      <th>{tu(label)}</th>
-      <td>{children}</td>
-    </tr>
+      <tbody>
+      <tr>
+        <th>{tu(label)}</th>
+        <td>{children}</td>
+      </tr>
+      </tbody>
   )
 }
