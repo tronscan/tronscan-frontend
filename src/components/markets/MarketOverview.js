7d5fd512f18a58a4a3bb4b6e2ab7e1bfbc40982d
zonLi <lizongkai@tron.network>
2018-07-04 17:17:07 +0800

    new table ， not finished yet

--- src/components/markets/MarketOverview.js
+++ src/components/markets/MarketOverview.js
@@ -4,6 +4,7 @@ import {Client} from "../../services/api";
 import {ExternalLink} from "../common/Links";
 import {FormattedNumber} from "react-intl";
 import {TRXPrice} from "../common/Price";
+import {Table, Input, Button, Icon} from 'antd';
 
 export default class MarketOverview extends Component {
   constructor() {
@@ -11,24 +12,129 @@ export default class MarketOverview extends Component {
 
     this.state = {
       markets: [],
+      filterDropdownVisible: false,
+      data: [],
+      columns: [],
+      searchText: '',
+      filtered: false,
     }
   }
 
   componentDidMount() {
-    // this.load();
+
   }
 
-  async load() {
-    let markets = await Client.getMarkets();
+  onInputChange = (e) => {
+    this.setState({searchText: e.target.value});
+  }
 
+  onSearch = () => {
+    let {markets} = this.props;
+    const {searchText} = this.state;
+    const reg = new RegExp(searchText, 'gi');
     this.setState({
-      markets,
+      filterDropdownVisible: false,
+      filtered: !!searchText,
+      data: this.data.map((record) => {
+        console.log(record);
+        const match = record.name.match(reg);
+        console.log(match);
+        if (!match) {
+          return null;
+        }
+        return {
+          ...record,
+          name: (
+              <span>
+              {record.name.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i')).map((text, i) => (
+                  text.toLowerCase() === searchText.toLowerCase()
+                      ? <span key={i} className="highlight">{text}</span> : text // eslint-disable-line
+              ))}
+            </span>
+          ),
+        };
+      }).filter(record => !!record),
     });
   }
 
-  render() {
 
+  setColumn = () => {
+    const columns = [
+      {
+        title: 'rank',
+        dataIndex: 'rank',
+        key: 'rank',
+        sorter: true
+      },
+      {
+        title: 'name',
+        dataIndex: 'name',
+        key: 'name',
+        filterDropdown: (
+            <div className="custom-filter-dropdown">
+              <Input
+                  ref={ele => this.searchInput = ele}
+                  placeholder="Search name"
+                  value={this.state.searchText}
+                  onChange={this.onInputChange}
+                  onPressEnter={this.onSearch}
+              />
+              <Button type="primary" onClick={this.onSearch}>Search</Button>
+            </div>
+        ),
+        filterIcon: <Icon type="smile-o" style={{color: this.state.filtered ? '#108ee9' : '#aaa'}}/>,
+        filterDropdownVisible: this.state.filterDropdownVisible,
+        onFilterDropdownVisibleChange: (visible) => {
+          console.log(visible);
+          this.setState({
+            filterDropdownVisible: visible,
+          }, () => {
+            this.searchInput && this.searchInput.focus()
+          });
+        },
+      },
+      {
+        title: 'pair',
+        dataIndex: 'pair',
+        key: 'pair',
+      },
+      {
+        title: 'volumeNative',
+        dataIndex: 'volumeNative',
+        key: 'volumeNative',
+      }
+    ];
+    return columns;
+  }
+  setData = (markets) => {
+    let data=[];
+    if(markets.length) {
+      markets.map((val) => {
+        data.push({
+          key: val.rank,
+          rank: val.rank,
+          name: val.name,
+          pair: val.pair,
+          volumeNative: val.volumeNative,
+        });
+
+      })
+    }
+  //  this.setState({data: data});
+    return data;
+  }
+
+  render() {
+    let columns = this.setColumn();
     let {markets} = this.props;
+    let data = this.state;
+    let tableData;
+    if(data.length){
+      tableData = this.state.data;
+    }
+    else{
+      tableData= this.setData(markets);
+    }
 
     function compare(property) {
       return function (obj1, obj2) {
@@ -43,11 +149,18 @@ export default class MarketOverview extends Component {
 
       }
     }
+
     markets = markets.sort(compare("rank")).slice(0, 99);
+    //console.log(markets);
+
 
     return (
+
+
         <div className="card">
-          <table className="table table-hover bg-white m-0 table-striped">
+          <Table columns={columns} dataSource={tableData}/>
+          {/*
+            <table className="table table-hover bg-white m-0 table-striped">
             <thead className="thead-dark">
               <tr>
                 <th style={{width: 25}}>{tu("rank")}</th>
@@ -85,7 +198,11 @@ export default class MarketOverview extends Component {
             }
             </tbody>
           </table>
+          */}
         </div>
+
     )
   }
 }
+
+
