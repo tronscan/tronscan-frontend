e9939148b029c82c155e4621442c3e674a39f06c
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-07-05 21:52:47 +0200

    add customized modules

--- src/components/network/NodeMap/NodeMap.js
+++ src/components/network/NodeMap/NodeMap.js
@@ -4,12 +4,12 @@ import L from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 import "leaflet.markercluster/dist/MarkerCluster.css";
-import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import "../../../styles/marker-cluster.scss";
 
 import "leaflet/dist/leaflet-src.js";
 import "leaflet.markercluster/dist/leaflet.markercluster-src.js";
-import "leaflet-echarts/src/leaflet-echarts.js";
-import "leaflet-echarts/lib/echarts.source.js";
+import "../../../lib/leaflet-echarts/leaflet-echarts.js";
+import "../../../lib/leaflet-echarts/lib/echarts.source";
 import config from '../../common/chart.config.js'
 
 export default class NodeMap extends Component {
@@ -22,22 +22,15 @@ export default class NodeMap extends Component {
     let data = [];
     config.mapChart.series[0].geoCoord = {};
     config.mapChart.series[1].markPoint.data = [];
-    if(nodes.length) {
-      for (let node in nodes) {
-        /*  if(nodes[node].city!=='') {
-            points.push([nodes[node].lat,nodes[node].lng,node]);
-            geoCoord[nodes[node].city] = [nodes[node].lng, nodes[node].lat];
-            data.push({name: nodes[node].city, ip: nodes[node].ip});
-          }
-        */
-        points.push([nodes[node].lat, nodes[node].lng, nodes[node].ip]);
-        geoCoord[nodes[node].ip] = [nodes[node].lng, nodes[node].lat];
-        data.push({name: nodes[node].ip, ip: nodes[node].ip});
-
+    if (nodes.length) {
+      for (let node of nodes) {
+        points.push([node.lat, node.lng, node.ip]);
+        geoCoord[node.ip] = [node.lng, node.lat];
+        data.push({name: node.ip, ip: node.ip});
       }
 
-      var addressPoints = points;
-      var map = L.map(this.$ref,{
+      let addressPoints = points;
+      let map = L.map(this.$ref, {
         maxZoom: 18,
         minZoom: 2,
         maxBounds: [
@@ -47,29 +40,31 @@ export default class NodeMap extends Component {
           [90, 180]
         ],
       });
-      var southWest = L.latLng(-90, -120);
-      var northEast = L.latLng(90, 120);
-      var bounds = L.latLngBounds(southWest, northEast);
+      let southWest = L.latLng(-90, -120);
+      let northEast = L.latLng(90, 120);
+      let bounds = L.latLngBounds(southWest, northEast);
 
-      var baseLayers = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiemtsaSIsImEiOiJjamhzbjFiZWYwNG9mM3ZwM3BpM2xudjBpIn0.BeVbGjUROg5szZiCmYZfnQ').addTo(map);
+      let baseLayers = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiemtsaSIsImEiOiJjamhzbjFiZWYwNG9mM3ZwM3BpM2xudjBpIn0.BeVbGjUROg5szZiCmYZfnQ').addTo(map);
 
 
       map.setView(L.latLng(0, 13.114129), 1);
-      var markers = L.markerClusterGroup();
+      let markers = L.markerClusterGroup();
 
-      for (var i = 0; i < addressPoints.length; i++) {
-        var a = addressPoints[i];
-        var title = a[2];
-        var marker = L.marker(new L.LatLng(a[0], a[1]), {title: title});
+      for (let i = 0; i < addressPoints.length; i++) {
+        let a = addressPoints[i];
+        let title = a[2];
+        let marker = L.marker(new L.LatLng(a[0], a[1]), {
+          title: title
+        });
         marker.bindPopup(title);
         markers.addLayer(marker);
       }
 
       map.addLayer(markers);
 
-      var overlay = new L.echartsLayer(map, echarts);
-      var chartsContainer = overlay.getEchartsContainer();
-      var myChart = overlay.initECharts(chartsContainer);
+      let overlay = new L.echartsLayer(map, echarts);
+      let chartsContainer = overlay.getEchartsContainer();
+      let myChart = overlay.initECharts(chartsContainer);
       window.onresize = myChart.onresize;
 
       config.mapChart.series[0].geoCoord = geoCoord;
@@ -79,19 +74,15 @@ export default class NodeMap extends Component {
     }
   }
 
-  componentDidUpdate() {
-
-  }
-
   render() {
 
     let {className} = this.props;
 
     return (
-        <div
-            style={{height: 600}}
-            className={className + " map-2d"}
-            ref={(cmp) => this.$ref = cmp}/>
+      <div
+        style={{height: 600}}
+        className={className + " map-2d"}
+        ref={(cmp) => this.$ref = cmp}/>
     )
   }
 }
