b377be1e50a9c350d0b539190ebbef8261479de4
PMD3VSolution <petermuchau@aol.com>
2018-07-14 11:23:50 +0200

    sv.js updated - update translation - Ledger Support Page

--- src/translations/sv.js
+++ src/translations/sv.js
@@ -15,8 +15,8 @@ export const messages = {
   ##################################################################################
   #                                                                                #
   #         Template for translating the tronscan.org website                      #
-  #         Version: 1.6-11072018                                                  #
-  #         Update Date: 11.07.2018                                                #
+  #         Version: 1.6-14072018                                                  #
+  #         Update Date: 14.07.2018                                                #
   #         Language: Swedish                                                      #
   #         Status: Approved Version                                               #
   #         Number of checks: 5                                                    #
@@ -678,24 +678,24 @@ export const messages = {
   ##################################################################################
   */
   "ledger_guide": "Ledger Guide",
-  "requirements": "Requirements",
-  "using_ledger_steps_message_0": "Using the Ledger with Tronscan requires the following steps:",
-  "a": "A",
-  "install_the": "Install the ",
+  "requirements": "Krav",
+  "using_ledger_steps_message_0": "Använda Ledger med Tronscan kräver följande steg:",
+  "a": "En",
+  "install_the": "Installera ",
   "app": "app",
-  "install_latest_version_message_0": "Install the latest version of ",
-  "opening_your_wallet": "Opening your wallet",
-  "open_the_tronscan_desktop_client": "Open the Tronscan Desktop client",
-  "click_open_wallet_top_right": "Click &ldquo;Open Wallet&rdquo; at the top right",
-  "choose_ledger": "Choose Ledger",
-  "connect_unlock_ledger_with_pin": "Connect and unlock your device with your PIN code",
-  "open_tron_app_on_ledger": "Open the Tron app on your Ledger",
-  "ledger_connected_tronscan_message_0": "If your Ledger is connected properly then Tronscan will show &ldquo;Connected&rdquo",
-  "click_on_open_wallet": "Click on &ldquo;Open Wallet&rdquo",
-  "redirect_to_your_wallet_message": "You will be redirected to your wallet and you can view your Tron address",
-  "using_ledger_to_sign_transactions": "Using the ledger to sign transactions",
-  "using_ledger_to_sign_transactions_message_0": "Make sure to keep the Ledger connected and the Tron app open to sign transactions using the Ledger while using Tronscan Desktop",
-  "using_ledger_to_sign_transactions_message_1": "Tronscan will show a popup with the transaction details when a transaction signature is required. Signatures are required whenever you vote, send TRX, change account name, etc..",
+  "install_latest_version_message_0": "Installera den senaste versionen av ",
+  "opening_your_wallet": "Öppna din wallet",
+  "open_the_tronscan_desktop_client": "Öppna Tronscan Desktop-klienten",
+  "click_open_wallet_top_right": "Klicka på knappen 'Öppna plånbok' längst upp till höger.",
+  "choose_ledger": "Välj Ledger",
+  "connect_unlock_ledger_with_pin": "Anslut och låsa upp din enhet med din PIN-kod",
+  "open_tron_app_on_ledger": "Öppna Tron-appen på din Ledger",
+  "ledger_connected_tronscan_message_0": "Om din Ledger är korrekt ansluten kommer Tronscan att visa 'Connected'",
+  "click_on_open_wallet": "Klicka på 'Open Wallet'",
+  "redirect_to_your_wallet_message": "Du kommer att omdirigeras till din plånbok och du kan se din Tron-adress",
+  "using_ledger_to_sign_transactions": "Använda bokstaven för att skriva transaktioner",
+  "using_ledger_to_sign_transactions_message_0": "Medan du använder Tronscan Desktop appen och signerar transaktionen kontrollerar du att bokstaven är ansluten och Tron-appen är öppen.",
+  "using_ledger_to_sign_transactions_message_1": "Tronscan visar en transaktionssignatur via en popup med transaktionsinformationen, om så krävs. Signering är nödvändig för följande åtgärder: Delta i SR-röst, skicka TRX, ändra kontonamn etc.",
   /*
   ##################################################################################
   #                                                                                #
