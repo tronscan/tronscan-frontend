f9c6121fb6912189c264c5122a3de50794a67a1a
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-07-30 12:23:33 +0200

    fix fields in participate asset issue

--- src/components/tools/TransactionViewer/Contract.js
+++ src/components/tools/TransactionViewer/Contract.js
@@ -46,6 +46,7 @@ export default function Contract({contract}) {
           </table>
         </Fragment>
       );
+
     case "PARTICIPATEASSETISSUECONTRACT":
       return (
           <Fragment>
@@ -56,9 +57,9 @@ export default function Contract({contract}) {
               </p>
             </div>
             <table className="table">
-              <Field label="from"><AddressLink address={contract.ownerAddress} /></Field>
-              <Field label="to"><AddressLink address={contract.toAddress} /></Field>
-              <Field label="amount">{contract.amount / ONE_TRX}</Field>
+              <Field label="To"><AddressLink address={contract.ownerAddress} /></Field>
+              <Field label="Issuer"><AddressLink address={contract.toAddress} /></Field>
+              <Field label="amount">{contract.amount}</Field>
               <Field label="token">{contract.token}</Field>
             </table>
           </Fragment>
