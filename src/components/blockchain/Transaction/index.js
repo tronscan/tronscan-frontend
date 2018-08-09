6834827264bd6fb991511b003f343237020b56e9
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-08-09 10:13:00 +0200

    wrap transaction data

--- src/components/blockchain/Transaction/index.js
+++ src/components/blockchain/Transaction/index.js
@@ -138,7 +138,7 @@ class Transaction extends React.Component {
                       (transaction.data && trim(transaction.data).length > 0) && <tr>
                         <th>{tu("data")}:</th>
                         <td>
-                          <pre className="border border-grey bg-light-grey m-0 p-2 rounded">
+                          <pre className="border border-grey bg-light-grey m-0 p-2 rounded" style={{whiteSpace: 'pre-wrap'}}>
                             {hextoString(transaction.data)}
                           </pre>
                         </td>
