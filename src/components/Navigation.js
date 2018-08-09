f5d7bb0d1d609f3dd952b75970e6e0ffcd078552
zonLi <lizongkai@tron.network>
2018-08-09 16:01:02 +0800

    representatives change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,7 @@ import {flatRoutes, routes} from "../routes"
 import {Link, NavLink, withRouter} from "react-router-dom"
 import {filter, find, isString, isUndefined, trim} from "lodash"
 import {tu,t} from "../utils/i18n"
-import {enableFlag, login, logout, setActiveCurrency, setLanguage, setTheme} from "../actions/app"
+import {enableFlag, login, loginWithAddress, logout, setActiveCurrency, setLanguage, setTheme} from "../actions/app"
 import {connect} from "react-redux"
 import {Badge} from "reactstrap"
 import Avatar from "./common/Avatar"
@@ -27,6 +27,7 @@ import Notifications from "./account/Notifications";
 import SendModal from "./transfer/Send/SendModal";
 import {bytesToString} from "@tronscan/client/src/utils/bytes";
 import {hexStr2byteArray} from "@tronscan/client/src/lib/code";
+import {isAddressValid} from "@tronscan/client/src/utils/crypto";
 import ReceiveModal from "./transfer/Receive/ReceiveModal";
 import {toastr} from 'react-redux-toastr'
 import {BarLoader} from "./common/loaders";
@@ -70,13 +71,13 @@ class Navigation extends PureComponent {
     if (trim(privateKey) === "external") {
       this.props.enableFlag("mobileLogin");
     } else {
-      this.props.login(privateKey);
+      this.props.loginWithAddress(privateKey);
     }
   };
 
   isLoginValid = () => {
     let {privateKey} = this.state;
-
+    console.log(privateKey);
     if (trim(privateKey) === "external") {
       return true;
     }
@@ -84,6 +85,9 @@ class Navigation extends PureComponent {
     if (!privateKey || privateKey.length === 0) {
       return false;
     }
+
+    if (isAddressValid(privateKey))
+      return true;
 
     if (privateKey.length !== 64) {
       return false;
@@ -647,6 +651,7 @@ const mapDispatchToProps = {
   setLanguage,
   logout,
   login,
+  loginWithAddress,
   setActiveCurrency,
   setTheme,
   enableFlag,
