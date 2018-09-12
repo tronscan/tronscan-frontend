d995f66d83b5e0fd7e43b3d36c3bdc1e0e11c998
zonLi <lizongkai@tron.network>
2018-09-12 11:57:12 +0800

    change styles

--- src/components/account/FreezeBalanceModal.js
+++ src/components/account/FreezeBalanceModal.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-restricted-globals */
 import {connect} from "react-redux";
+import {injectIntl} from "react-intl";
 import React from "react";
 import {Modal, ModalBody, ModalHeader} from "reactstrap";
 import {tu, t} from "../../utils/i18n";
@@ -73,26 +74,27 @@ class FreezeBalanceModal extends React.PureComponent {
   render() {
 
     let {amount, confirmed, loading} = this.state;
-    let {trxBalance,frozenTrx} = this.props;
+    let {trxBalance,frozenTrx, intl} = this.props;
 
     let isValid = !loading && (amount > 0 && trxBalance >= amount && confirmed);
     return (
         <Modal isOpen={true} toggle={this.hideModal} fade={false} className="modal-dialog-centered">
-          <ModalHeader className="text-center" toggle={this.hideModal}>
+          <ModalHeader className="text-center _freezeHeader" toggle={this.hideModal}>
             {tu("freeze")}
           </ModalHeader>
           <ModalBody className="text-center">
             <form>
               <div className="form-group">
-                <div className="text-left">{tu("current_power")}: <span style={{fontWeight: 800}}>{frozenTrx/ONE_TRX}</span>
+                <div className="text-left _power">{tu("current_power")}: <span style={{fontWeight: 800}}>{frozenTrx/ONE_TRX}</span>
                 </div>
-                <label>{tu("trx_amount")}</label>
 
                 <NumberField
                     min={1}
                     decimals={0}
                     value={amount}
-                    className="form-control text-center"
+                    placeholder={intl.formatMessage({id:'trx_amount'})}
+                    className="form-control text-left"
+                    style={{marginTop:'12px',background:"#F3F3F3",border:"1px solid #EEEEEE"}}
                     onChange={this.onAmountChanged}/>
               </div>
 
@@ -100,7 +102,7 @@ class FreezeBalanceModal extends React.PureComponent {
                 <input type="checkbox"
                        className="form-check-input"
                        onChange={(ev) => this.setState({confirmed: ev.target.checked})}/>
-                <label className="form-check-label">
+                <label className="form-check-label _freeze">
                   {tu("token_freeze_confirm_message_0")} <b><FormattedNumber
                     value={amount}/> TRX</b> {t("token_freeze_confirm_message_1")}
                 </label>
@@ -109,6 +111,7 @@ class FreezeBalanceModal extends React.PureComponent {
                 <button className="btn btn-primary col-sm"
                         disabled={!isValid}
                         onClick={this.freeze}
+                        style={{background:'#4A90E2',borderRadius:'0px',border:'0px'}}
                 >
                   <i className="fa fa-snowflake mr-2"/>
                   {tu("freeze")}
@@ -133,4 +136,4 @@ const mapDispatchToProps = {
   reloadWallet
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(FreezeBalanceModal)
+export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(FreezeBalanceModal))
