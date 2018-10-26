ae88e53cc6ffb325de66e488ccac147af6a4fd9f
Harry <431027103@qq.com>
2018-10-26 11:58:16 +0800

    from

--- src/components/account/CreateTxnPairModal.js
+++ src/components/account/CreateTxnPairModal.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {tu} from "../../utils/i18n";
 import {Modal, ModalBody, ModalHeader} from "reactstrap";
+import _ from "lodash";
 
 class CreateTxnPairModal extends Component {
 
@@ -11,10 +12,90 @@ class CreateTxnPairModal extends Component {
         this.state = {
             name: "",
             disabled: false,
+            formTxnPair: {
+                txnpair_name_1: {
+                    valid: false,
+                    value: "",
+                    balance:'',
+                    error: ''
+                },
+                txnpair_name_2: {
+                    valid: false,
+                    value: '',
+                    balance:'',
+                    error: ''
+                },
+                txnpair_amount_1: {
+                    valid: false,
+                    value:'',
+                    balance:'',
+                    error: ''
+                },
+                txnpair_amount_2: {
+                    valid: false,
+                    value: '',
+                    balance:'',
+                    error: ''
+                }
+            },
         };
     }
 
-    isValid = () => {
+    handleVerifyCodeChange(field, value) {
+        const {formTxnPair: {txnpair_name_1,txnpair_name_2,txnpair_amount_1,txnpair_amount_2}} = this.state;
+        let {tokenBalances} = this.props;
+        const newFieldObj = {value, valid: true, balance:'', error: ''};
+        const contractList = {
+            txnpair_name_1() {
+                 if (value) {
+                     newFieldObj.error = '**Required';
+                     newFieldObj.valid = false;
+                 }else{
+                     tokenBalances = _(tokenBalances)
+                         .filter(tb => tb.name.toUpperCase() === value.name)
+                         .value();
+                     newFieldObj.balance = tokenBalances[0].balance;
+                 }
+            },
+            txnpair_name_2() {
+                if (value) {
+                    newFieldObj.error = '**Required';
+                    newFieldObj.valid = false;
+                }
+            },
+            txnpair_amount_1() {
+                if (value) {
+                    newFieldObj.error = '**Required';
+                    newFieldObj.valid = false;
+                }else{
+
+                }
+            },
+            txnpair_amount_2() {
+                if (value.length) {
+                    newFieldObj.error = '**Required';
+                    newFieldObj.valid = false;
+                }
+            },
+        }
+        if(field == 'contract_address' || field == 'contract_code'){
+            contractList[field]()
+        }
+
+        if(field == 'contract_optimization'){
+            newFieldObj.value = newFieldObj.value == 'true'
+        }
+
+        this.setState({
+            formVerify: {
+                ...this.state.formVerify,
+                [field]: newFieldObj
+            }
+        });
+    }
+
+    isValid = (type) => {
+
         let {name} = this.state;
 
         if (name.length < 8) {
@@ -72,9 +153,11 @@ class CreateTxnPairModal extends Component {
                         <div className="col-md-6">
                             <label>{tu("通证名称")}</label>
                             <select className="custom-select"
-                                //value={selectedResource}
-                                // onChange={(e) => {this.resourceSelectChange(e.target.value)}}
+                                value={}
+                                onChange={(e) => {this.resourceSelectChange(e.target.value)}}
+
                             >
+                                <option value=''>{t("请选择通证名称")}</option>
                             </select>
                             <div className="invalid-feedback text-center text-danger">
                                 {errorMessage}
