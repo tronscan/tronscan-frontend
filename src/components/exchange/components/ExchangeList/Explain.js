74dcebd0778cb411b0c00faf97e84bd91ae19a0c
jizhiqiang <jizhiqiang@tron.network>
2018-11-08 15:15:51 +0800

    fix color

--- src/components/exchange/components/ExchangeList/Explain.js
+++ src/components/exchange/components/ExchangeList/Explain.js
@@ -8,7 +8,7 @@ export function Explain() {
     <div className="mb-3">{tu("token_application_instructions_title")}</div>
    
     <div className="exchange-list-explain__content p-2">
-      <p>{tu("token_application_instructions_1")} {tu("token_application_instructions_2")}
+      <p style={{color: '#666'}}>{tu("token_application_instructions_1")} {tu("token_application_instructions_2")}
         <a href="https://goo.gl/forms/OXFG6iaq3xXBHgPf2
 " target="_blank">{tu("click_here_to_apply")}</a>
       </p>
