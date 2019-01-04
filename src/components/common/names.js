b72cf562bc14256a0ac61974015dc9c295b7fba0
jizhiqiang <jizhiqiang@tron.network>
2019-01-04 14:35:35 +0800

    change names.js

--- src/components/common/names.js
+++ src/components/common/names.js
@@ -9,7 +9,7 @@ export const NameWithId = ({value}) => {
     <span>
       <FormattedNumber value={value.map_amount}/>
       <span className="mx-1">{value.map_token_name}</span>
-      <span style={{color: '#808080'}}>[ID:{value.map_token_id}]</span>
+      {value.map_token_id != 0 && <span style={{color: '#808080'}}>[ID:{value.map_token_id}]</span>}
     </span>
   );
 }
