b362f2e16009bbe02918ae6d32179d22f21fb8f4
Harry <431027103@qq.com>
2019-01-09 20:26:11 +0800

    fixbug

--- src/utils/rebuildList.js
+++ src/utils/rebuildList.js
@@ -16,14 +16,14 @@ export default (list=[], tokenId, amount) =>{
         const id = item[tokenId]
         
         if(id == '_' || upperCase(id) == "TRX"){
-          setItem(item, 'TRX', id, 6, amount?item[amount] / Math.pow(10,6): 0)
+          setItem(item, 'TRX', id, 6, amount?item[amount] / Math.pow(10,6): 0,'TRX')
         }
         if(IDmap[id]){
           const list = IDmap[id].split('_')
-          setItem(item, list[0], list[1], list[2], amount? item[amount] / Math.pow(10,list[2]): 0)
+          setItem(item, list[0], list[1], list[2], amount? item[amount] / Math.pow(10,list[2]): 0,list[3])
         }
         if(!IDmap[id] && id != "_" && upperCase(id) != "TRX"){
-          setItem(item, item[tokenId], 0, 0, item[amount])
+          setItem(item, item[tokenId], 0, 0, item[amount],item[tokenId])
         }
 
         return item
@@ -34,14 +34,14 @@ export default (list=[], tokenId, amount) =>{
           const id = item[tid]
           
           if(id == '_' || upperCase(id) == "TRX"){
-            setItem(item, 'TRX', id, 6, amount[index]?item[amount[index]] / Math.pow(10,6): 0,index)
+            setItem(item, 'TRX', id, 6, amount[index]?item[amount[index]] / Math.pow(10,6): 0,'TRX',index)
           }
           if(IDmap[id]){
             const list = IDmap[id].split('_')
-            setItem(item, list[0], list[1], list[2], amount[index]? item[amount[index]] / Math.pow(10,list[2]): 0,index)
+            setItem(item, list[0], list[1], list[2], amount[index]? item[amount[index]] / Math.pow(10,list[2]): 0,list[3],index)
           }
           if(!IDmap[id] && id != "_" && upperCase(id) != "TRX"){
-            setItem(item, item[tid], 0, 0, item[amount[index]],index)
+            setItem(item, item[tid], 0, 0, item[amount[index]],item[tid],index)
           }
         })
 
@@ -56,11 +56,13 @@ export default (list=[], tokenId, amount) =>{
   }
 }
 
-function setItem(item,name,id,pre,amount,index=''){
+function setItem(item,name,id,pre,amount,abbr,index=''){
   index = index == 0? '': index
   item['map_token_name'+index] = name
+  item['map_token_name_abbr'+index] = abbr
   item['map_token_id'+index] = id
   item['map_token_precision'+index] = pre
   item['map_amount'+index] =amount
+
   return item
 }
