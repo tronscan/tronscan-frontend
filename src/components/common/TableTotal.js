dba5e673870acc38d1102e967cbd4cf5ad10806d
Harry <431027103@qq.com>
2019-03-06 12:13:15 +0800

    Build

--- src/components/common/TableTotal.js
+++ src/components/common/TableTotal.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { injectIntl } from "react-intl";
+import {t, tu} from "../../utils/i18n";
 import {QuestionMark} from "./QuestionMark";
 
 class TotalInfo extends React.Component {
@@ -15,7 +16,7 @@ class TotalInfo extends React.Component {
         let {total, rangeTotal, typeText,common = false, intl, markName='table-question-mark'} = this.props;
         let tableInfoSmall = intl.formatMessage({id: 'view_total'}) + ' ' + rangeTotal + ' ' + intl.formatMessage({id: typeText});
         let tableInfoBig = intl.formatMessage({id: 'view_total'}) + ' ' + rangeTotal + ' ' + intl.formatMessage({id: typeText}) + '<br/>' +  '(' + intl.formatMessage({id: 'table_info_big'}) + ')';
-        let tableInfo =  rangeTotal > 10000? tableInfoBig: tableInfoSmall;
+        let tableInfo =  rangeTotal > 10000? tableInfoBig : tableInfoSmall;
         let tableInfoTipSmall = intl.formatMessage({id: 'table_info_big_tip1'}) + ' ' + rangeTotal + ' ' + intl.formatMessage({id: 'table_info_big_tip2'}) + intl.formatMessage({id: 'table_info_big_tip4'});
         let tableInfoTipBig = intl.formatMessage({id: 'table_info_big_tip1'}) + ' ' + rangeTotal + ' ' + intl.formatMessage({id: 'table_info_big_tip2'}) + intl.formatMessage({id: 'table_info_big_tip3'}) + intl.formatMessage({id: 'table_info_big_tip4'});
         let tableInfoTip = rangeTotal > 10000? tableInfoTipBig: tableInfoTipSmall;
@@ -27,8 +28,13 @@ class TotalInfo extends React.Component {
                             <QuestionMark placement="top" text="to_provide_a_better_experience"></QuestionMark>
                     </span>
                     </div>:<div className="table_pos_info d-none d-md-block" style={{left: 'auto'}}>
-                        <span dangerouslySetInnerHTML={{__html:tableInfo}}></span>
-                        <span className={rangeTotal > 10000? markName:"table-question-mark-small"}><QuestionMark placement="top" info={tableInfoTip} ></QuestionMark></span>
+                        {
+                            rangeTotal > 10000?
+                               <div>{tu('view_total')} {rangeTotal} {tu(typeText)} <QuestionMark placement="top" info={tableInfoTip} ></QuestionMark> <br/> <span>{tu('table_info_big')}</span></div>
+                             : <span>{tu('view_total')} {rangeTotal} {tu(typeText)} <QuestionMark placement="top" info={tableInfoTip} ></QuestionMark></span>
+
+                        }
+                        <span className={rangeTotal > 10000? markName:"table-question-mark-small"}></span>
                     </div>
                 }
             </Fragment>
