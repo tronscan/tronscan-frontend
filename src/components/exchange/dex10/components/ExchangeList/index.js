679b676179f890d2c894400d66aee8af1f625ef4
Harry <431027103@qq.com>
2019-04-18 17:44:46 +0800

    Countdown

--- src/components/exchange/dex10/components/ExchangeList/index.js
+++ src/components/exchange/dex10/components/ExchangeList/index.js
@@ -86,10 +86,31 @@ class ExchangeList extends React.Component {
       let sec = parseInt((msec / 1000) % 60);
 
       // 个位数前补零
-      day = day > 9 ? day : "0" + day;
-      hr = hr > 9 ? hr : "0" + hr;
-      min = min > 9 ? min : "0" + min;
-      sec = sec > 9 ? sec : "0" + sec;
+      if(day < 10 &&  day > 0){
+          day =  "0" + day
+      }else if(day <= 0){
+          day =  "00"
+      }
+      if(hr < 10 &&  hr > 0){
+          hr =  "0" + hr
+      }else if(hr <= 0){
+          hr =  "00"
+      }
+      if(min < 10 &&  min > 0){
+          min =  "0" + min
+      }else if(min <= 0){
+          min =  "00"
+      }
+      if(sec < 10 &&  sec > 0){
+          sec =  "0" + sec
+      }else if(sec <= 0){
+          sec =  "00"
+      }
+
+      // day = day > 9 ? day :"0" + day ;
+      // hr = hr > 9 ? hr : "0" + hr;
+      // min = min > 9 ? min : "0" + min;
+      // sec = sec > 9 ? sec : "0" + sec;
       this.setState({
           day: day,
           hr: hr,
