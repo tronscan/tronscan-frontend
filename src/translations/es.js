7bcb274a6ff4d1a06f2d4c886037cc3f7d8fc67f
Harry <431027103@qq.com>
2019-05-24 14:35:10 +0800

    Build

--- src/translations/es.js
+++ src/translations/es.js
@@ -1773,7 +1773,9 @@ export const messages = {
   "footer_fellow_us":"Follow Us",
   "footer_developer_resources":"Developer Resources",
   "footer_support_center":"Support Center",
-  "footer_tron_network":"TRON"
+  "footer_tron_network":"TRON",
+  "login_privatekey_warn":"In order to ensure account security and avoid asset loss, please take good care of your private key and never give it away to anyone. It is recommended to use ",
+  "login_privatekey_warn_safe":" to login",
 
 
 
