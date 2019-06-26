80288fab878cd84a4f813b7e02861120455706ab
Harry <431027103@qq.com>
2019-06-26 19:33:11 +0800

    Build

---
+++ config/webpack.dll.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require("webpack");
+module.exports = {
+    entry:{
+        react:['react','react-dom',],
+        polyfill:['whatwg-fetch'],
+        ui: ["antd"],
+        base: ["lodash", "moment"],
+        charts:["echarts","highcharts"],
+
+    },
+    output:{
+        path: path.join(__dirname, "../public/dll"),
+        filename: "[name].dll.js",
+        library: "[name]"
+    },
+    plugins:[
+        new webpack.DllPlugin({
+            path: path.join(__dirname, "../mainfest", "[name]-manifest.json"),
+            name: "[name]"
+        })
+    ]
+}
