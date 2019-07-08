90bb7bdd728715afdb6cb6d9e6ed3460470ca0d6
Harry <431027103@qq.com>
2019-07-08 19:26:03 +0800

    update url

--- src/components/blockchain/Contract/Code.js
+++ src/components/blockchain/Contract/Code.js
@@ -50,7 +50,7 @@ export default class Code extends React.Component {
   }
   async getContractVerifyStatus () {
     let { filter } = this.props;
-    let { data } = await xhr.post(`http://172.16.21.246:9016/v1/api/contract/info`, {
+    let { data } = await xhr.post(`http://18.219.114.60:9016/v1/api/contract/info`, {
       "contractAddress": filter.address,
     }).catch(function (e) {
       console.log(e)
