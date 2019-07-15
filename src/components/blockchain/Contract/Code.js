43e1e9db1ede23e290f88488f0aeab8cf12e712f
Harry <431027103@qq.com>
2019-07-15 11:25:37 +0800

    code

--- src/components/blockchain/Contract/Code.js
+++ src/components/blockchain/Contract/Code.js
@@ -24,11 +24,11 @@ export default class Code extends React.Component {
 
   constructor(props) {
     super(props);
-    // https://api.shasta.trongrid.io   https://api.trongrid.io
+    // https://api.shasta.trongrid.io
     this.tronWeb = new tronWeb({
-      fullNode: 'https://api.shasta.trongrid.io',
-      solidityNode: 'https://api.shasta.trongrid.io',
-      eventServer: 'https://api.shasta.trongrid.io',
+      fullNode: 'https://api.trongrid.io',
+      solidityNode: 'https://api.trongrid.io',
+      eventServer: 'https://api.trongrid.io',
     })
     this.state = {
       loading: true,
