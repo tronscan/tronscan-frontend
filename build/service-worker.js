9b5d6175d0fea0e5206f7591022206d9180deb6c
Harry <431027103@qq.com>
2019-08-29 14:55:18 +0800

    Update build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48a84d74ca7bf736b6458c525079ed9c.js"
+  "/precache-manifest.3c32abdba8a2bfc3fce67b188b4f44ae.js"
 );
 
 self.addEventListener('message', (event) => {
