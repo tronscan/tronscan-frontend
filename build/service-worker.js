496dee712bd4c12d678c1c6b2fc057965b2e4a1a
wangyan <346319096@qq.com>
2019-09-10 11:27:34 +0800

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d0f700cf76b25be6fbcdcbc5240e3e37.js"
+  "/precache-manifest.1957ec52a4e3c39be7a0ec00a34d0b7b.js"
 );
 
 self.addEventListener('message', (event) => {
