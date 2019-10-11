6b22e04c60ce667b1b159d3901b07888d33507e2
wangyan <346319096@qq.com>
2019-10-11 16:17:52 +0800

    delect debugger

--- src/utils/jsencrypt.js
+++ src/utils/jsencrypt.js
@@ -6,8 +6,8 @@ import { JSENCRYPTKEY } from './../constants';
  * jsencrypt
 */
 export const jsencrypt = value => {
+    console.log(value, '===')
     let key = new NodeRSA(JSENCRYPTKEY);
     const encrypted = key.encrypt(value, 'base64');
-    debugger
     return encrypted;
 };
