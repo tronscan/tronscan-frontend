9b706d470e9403e77d94dd110cc203369dc97fd1
yq <11@163.com>
2019-10-17 17:56:08 +0800

    delete some splitChunks

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -251,21 +251,7 @@ module.exports = function(webpackEnv) {
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
         chunks: 'all',
-        //name: false,
-        minSize: 30000,
-        minChunks: 1,
-        maxAsyncRequests: 5,
-        maxInitialRequests: 3,
-        name: true,
-        cacheGroups: {
-            styles: {
-                name: 'style',
-                test: /\.css$/,
-                chunks: 'all',
-                enforce: true
-            }
-        }
-
+        name: false,
       },
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
