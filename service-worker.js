63e6e83ef10ed4d45717e1eecb155e627334e64e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-27 09:49:37 +0000

    Deploy tronscan/tronscan-frontend to github.com/tronscan/tronscan-frontend.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://tronscan.github.io/tronscan-frontend/precache-manifest.618bb3288a419314a50dd5141affdad8.js"
+  "https://tronscan.github.io/tronscan-frontend/precache-manifest.86fcd56ffe7796761bc7e7c1ad4d5a91.js"
 );
 
 self.addEventListener('message', (event) => {
