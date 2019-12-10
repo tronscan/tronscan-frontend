f082972e83dc3da0f69aa84ea5e39472fae6a493
yq <11@163.com>
2019-12-10 14:54:05 +0800

    hide Poloni DEX and sunnetwork

--- src/routes.js
+++ src/routes.js
@@ -360,49 +360,49 @@ export const routes = [
     //   showInMenu: false,
     //   none: true
     // },
-    {
-        path: "/network",
-        label: "nav_network",
-        icon: "fas fa-project-diagram",
-        routes: [
-            [
-                "sun_network_product",
-                {
-                    url: NETURL.SUNNET,
-                    icon: "fas fa-columns",
-                    label: "sun_network_DAppChain"
-                }
-            ],
-            [
-                "sun_network_relevant_information",
-                {
-                    url: "https://tron.network/sunnetwork/#/",
-                    icon: "fa fa-globe",
-                    label: "sun_network_project_introduction"
-                },
-                {
-                    url: "https://tron.network/sunnetwork/doc/guide/",
-                    icon: "fa fa-book",
-                    label: "sun_network_development_document"
-                },
-                {
-                    url:
-                        "https://support.tronscan.org/hc/en-us/articles/360035944072-SUN-Network-Developer-Challenge-Starts",
-                    icon: "fas fa-award",
-                    label: "developer_challenge"
-                }
-            ]
-        ]
-    },
-    {
-        label: "Poloni DEX",
-        path: "/exchange/trc20",
-        icon: "fas fa-exchange-alt",
-        component: Exchange20,
-        isExact: true,
-        none: true,
-        showInMenu: IS_MAINNET ? true : false
-    },
+    // {
+    //     path: "/network",
+    //     label: "nav_network",
+    //     icon: "fas fa-project-diagram",
+    //     routes: [
+    //         [
+    //             "sun_network_product",
+    //             {
+    //                 url: NETURL.SUNNET,
+    //                 icon: "fas fa-columns",
+    //                 label: "sun_network_DAppChain"
+    //             }
+    //         ],
+    //         [
+    //             "sun_network_relevant_information",
+    //             {
+    //                 url: "https://tron.network/sunnetwork/#/",
+    //                 icon: "fa fa-globe",
+    //                 label: "sun_network_project_introduction"
+    //             },
+    //             {
+    //                 url: "https://tron.network/sunnetwork/doc/guide/",
+    //                 icon: "fa fa-book",
+    //                 label: "sun_network_development_document"
+    //             },
+    //             {
+    //                 url:
+    //                     "https://support.tronscan.org/hc/en-us/articles/360035944072-SUN-Network-Developer-Challenge-Starts",
+    //                 icon: "fas fa-award",
+    //                 label: "developer_challenge"
+    //             }
+    //         ]
+    //     ]
+    // },
+    // {
+    //     label: "Poloni DEX",
+    //     path: "/exchange/trc20",
+    //     icon: "fas fa-exchange-alt",
+    //     component: Exchange20,
+    //     isExact: true,
+    //     none: true,
+    //     showInMenu: IS_MAINNET ? true : false
+    // },
     // {
     //   label: "Poloni DEX",
     //   path: "/exchange/:type",
