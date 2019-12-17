560af7a0f9d58248c5706e71a0dc3382a5799f1d
yq <11@163.com>
2019-12-17 15:54:38 +0800

    nile build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3179aa70243617a19c4e2c9382b3f06.js"
+  "/precache-manifest.d4ed1b10ce777661a4c7a6869f619e9e.js"
 );
 
 self.addEventListener('message', (event) => {
