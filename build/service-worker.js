de287de91d19ea7f2cdd5ea6d024ef4bdf324a66
leon <48722456@qq.com>
2019-12-18 18:18:24 +0800

    token detail market style

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,23 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-<<<<<<< Updated upstream
-  "/precache-manifest.55b69d33179ef9664157b93db4d1903b.js"
-=======
-<<<<<<< HEAD
-  "/precache-manifest.7ac79e2cf09bb593e0c653f49c374da0.js"
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-  "/precache-manifest.4fbce0abe120104a3a1dd46895fc8b26.js"
-=======
-  "/precache-manifest.b313836a96af5aba92b702e43f99a588.js"
->>>>>>> origin/master
-=======
-  "/precache-manifest.ab4704e2a188765d902ebcaf89897e4a.js"
->>>>>>> origin/master
->>>>>>> origin/feature/freeze-charts
->>>>>>> Stashed changes
+  "/precache-manifest.15c579c964045a6f7916852ca4e6835a.js"
 );
 
 self.addEventListener('message', (event) => {
