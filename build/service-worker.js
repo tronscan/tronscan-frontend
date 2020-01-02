8800a60074311db0262bb7e3729bda0759f3dd9f
Harry <431027103@qq.com>
2020-01-02 15:07:25 +0800

    Set gtag config

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f850a21f4ab2b8a6ead82bad3b773bc8.js"
+  "/precache-manifest.d39d4bc01e3319ce0eb4400edead5e2c.js"
 );
 
 self.addEventListener('message', (event) => {
