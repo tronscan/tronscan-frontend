f949ffde0a37074783eb343b359bcc15243ea97f
leon <48722456@qq.com>
2020-01-09 11:47:51 +0800

    internal trans test build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36db818a29494a11ddaebf118c38067d.js"
+  "/precache-manifest.6e2167acfe7cde948085263af62d96f4.js"
 );
 
 self.addEventListener('message', (event) => {
