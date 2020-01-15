bc193f9d5d627fe9900ec9da739e2a2af0d62e7f
xueyuanying <875979119@qq.com>
2020-01-15 15:07:58 +0800

    accounts

--- src/components/Data/TopData/Accounts.js
+++ src/components/Data/TopData/Accounts.js
@@ -1,21 +1,51 @@
-import React, { Fragment }  from 'react';
+import React, { Fragment } from "react";
+import ApiClientData from "../../../services/dataApi";
 
-
-class Accounts extends React.Component{
-  constructor(){
+class Accounts extends React.Component {
+  constructor() {
     super();
+    this.state = {
+      sendTrxNumbers: [],
+      sendTrxItems: [],
+      receiveTrxNumbers: [],
+      receiveTrxItems: [],
+      freezeTrxNumbers: [],
+      voteTrxNumbers: []
+    };
   }
-  componentDidMount(){
-
+  componentDidMount() {
+    this.getData();
   }
-  
 
+  async getData() {
+    let sendTrxNumbers = await ApiClientData.getTop10Data({ type: 1, time: 1 });
+    let sendTrxItems = await ApiClientData.getTop10Data({ type: 2, time: 1 });
+    let receiveTrxNumbers = await ApiClientData.getTop10Data({
+      type: 3,
+      time: 1
+    });
+    let voteTrxNumbers = await ApiClientData.getTop10Data({ type: 4, time: 1 });
+    this.setState({
+      sendTrxNumbers,
+      sendTrxItems,
+      receiveTrxNumbers,
+      voteTrxNumbers
+    });
+  }
 
-  render(){
+  render() {
+    let {
+      sendTrxNumbers,
+      sendTrxItems,
+      receiveTrxNumbers,
+      voteTrxNumbers
+    } = this.state;
     return (
-        <div><span>1</span></div>
-    )
+      <div>
+        <span>1</span>
+      </div>
+    );
   }
 }
 
-export default Accounts
+export default Accounts;
