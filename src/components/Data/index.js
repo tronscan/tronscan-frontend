4a0af83be432fa0421ef91ec68b23e1c58be2a3e
leon <48722456@qq.com>
2020-01-15 14:26:31 +0800

    best data

---
+++ src/components/Data/index.js
@@ -0,0 +1,21 @@
+import React, { Fragment }  from 'react';
+
+
+class BestData extends React.Component{
+  constructor(){
+    super();
+  }
+  componentDidMount(){
+
+  }
+
+  setTabs(){
+    
+  }
+
+  render(){
+    return ''
+  }
+}
+
+export default BestData
