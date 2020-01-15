2d3717a1b137f3c18796b63d7de7c60d6de2a82f
leon <48722456@qq.com>
2020-01-15 14:49:44 +0800

    best data tabs

--- src/components/Data/index.js
+++ src/components/Data/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment }  from 'react';
-
-
+import { tu } from "../../utils/i18n";
+import Overview from "./Overview"
 class BestData extends React.Component{
   constructor(){
     super();
@@ -10,11 +10,64 @@ class BestData extends React.Component{
   }
 
   setTabs(){
-    
+    this.setState(prevProps => ({
+      loading: false,
+      tabs: {
+        ...prevProps.tabs,
+        overview: {
+          id: "Overview",
+          path: "",
+          label: <span>{tu("data_overview")}</span>,
+          cmp: () => (
+            <Overview />
+          )
+        },
+        accounts: {
+          id: "",
+          path: "",
+          label: <span>{tu("data_account")}</span>,
+          cmp: () => (
+            <Overview />
+          )
+        },
+        tokens: {
+          id: "",
+          path: "",
+          label: <span>{tu("data_token")}</span>,
+          cmp: () => (
+            <Overview />
+          )
+        },
+        contracts: {
+          id: "",
+          path: "",
+          label: <span>{tu("data_contract")}</span>,
+          cmp: () => (
+            <Overview />
+          )
+        },
+        resources: {
+          id: "",
+          path: "",
+          label: <span>{tu("data_recourse")}</span>,
+          cmp: () => (
+            <Overview />
+          )
+        }
+      }
+    }))
   }
 
   render(){
-    return ''
+    return (
+      <main className="container header-overlap token_black">
+        <div className="row">
+          <div className="col-md-12 ">
+
+          </div>
+        </div>
+      </main>
+    )
   }
 }
 
