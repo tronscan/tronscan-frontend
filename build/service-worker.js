aab8e68674104211075b6739166138a19a148bad
liuximing <459794155@qq.com>
2020-01-19 15:07:06 +0800

    ant design backtop and help link position change build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca40295aa5341116b80f5a053213b7d0.js"
+  "/precache-manifest.06a62afb183e5a20ca71c5a57eb04895.js"
 );
 
 self.addEventListener('message', (event) => {
