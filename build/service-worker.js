03196d001dae7d29cc625c131ca99a52a9506269
Harry <431027103@qq.com>
2020-01-28 11:27:08 +0800

    Set online config and hide zendesk

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bc4915c847a4c8aaa986421a64764e8.js"
+  "/precache-manifest.ff5c5e216076f0ca7aad29c4bf8a4f91.js"
 );
 
 self.addEventListener('message', (event) => {
