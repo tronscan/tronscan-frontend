e5115707e556cdaffb26bf88b726fcc307aec1a4
yq <11@163.com>
2020-02-17 11:43:54 +0800

    build for zendesk

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e53424f7a5ac03fa52ca9c23aeeb7b1.js"
+  "/precache-manifest.98f38e5e42220ad5abb35f0b6f6cb8e9.js"
 );
 
 self.addEventListener('message', (event) => {
