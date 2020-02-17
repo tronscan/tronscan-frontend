82f5fed0359ae5cc8dfb6418b7b31172800f0573
yq <11@163.com>
2020-02-17 11:35:07 +0800

    change ip for test

--- src/constants.js
+++ src/constants.js
@@ -12,7 +12,7 @@ export const NETURL = {
   // SUNNET: "https://debugdappchain.tronscan.org",
   NEWMAINNET: "https://tronscan.io",
   NEWSUNNET: "https://dappchain.tronscan.io",
-  MAINNET: "https://tronscan.org",
+  MAINNET: "http://testhttps.tronscan.org/",
   SUNNET: "https://dappchain.tronscan.org",
   // MAINNET: "http://18.217.215.94:65",
   // SUNNET: "http://18.217.215.94:66",
