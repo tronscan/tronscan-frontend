0398090f4a825d70cff48b1f06ded7339d79c30e
xueyuanying <875979119@qq.com>
2020-02-27 10:47:50 +0800

    time test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59ebf449fa11f25a321dc446f08d6ca5.js"
+  "/precache-manifest.59a5ef4f805f700a2df212eea727c455.js"
 );
 
 self.addEventListener('message', (event) => {
