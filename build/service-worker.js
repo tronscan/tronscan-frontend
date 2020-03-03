d50de22cb1e4fed3e64d3f3b8c164df775dbb455
Harry <431027103@qq.com>
2020-03-03 19:06:50 +0800

    build for test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97558e619fdc057440b263427df52d80.js"
+  "/precache-manifest.629dbb58ccd361c096f3c65a4fd297f1.js"
 );
 
 self.addEventListener('message', (event) => {
