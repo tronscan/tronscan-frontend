a45c4a512e7c571ae561d30294571331e8a54856
yq <11@163.com>
2020-03-06 14:26:04 +0800

    build for test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d3ee06b50e6334291df85b86a141c50c.js"
+  "/precache-manifest.ce5d0bd55321cc83ad176f404933adef.js"
 );
 
 self.addEventListener('message', (event) => {
