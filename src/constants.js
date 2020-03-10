02f0cce87dd0c334c16acecad4b885a2b28631b6
yq <11@163.com>
2020-03-10 14:51:17 +0800

    build for test

--- src/constants.js
+++ src/constants.js
@@ -17,7 +17,7 @@ export const IS_NILE = true;
 
 
 export const NETURL = {
-    MAINNET: "https://nile.tronscan.org",
+    MAINNET: "http://testhttps.tronscan.org",
     SUNNET: "https://dappchain.tronscan.org",
     NEWMAINNET: "https://tronscan.io",
     NEWSUNNET: "https://dappchain.tronscan.io",
