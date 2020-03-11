94b0a6962b205e55b3414adda4a42bfa4fa08602
liuximing <459794155@qq.com>
2020-03-11 16:06:49 +0800

    btt usdt price update

--- src/components/tokens/TokenDetail/BTTSupply.js
+++ src/components/tokens/TokenDetail/BTTSupply.js
@@ -91,18 +91,18 @@ class BTTSupply extends React.Component {
         let BTCData= await xhr.post(`${API_URL}/api/system/proxy`,{
             url:BTCWinkTronbetURL
         });
-        let trxPriceDataUSD = BTCData.data &&
-        BTCData.data.data.BTT &&
-        BTCData.data.data.BTT.quote &&
-        BTCData.data.data.BTT.quote.BTC &&
-        BTCData.data.data.BTT.quote.BTC.price
+        let trxPriceDataUSD = USDData.data &&
+        USDData.data.data.BTT &&
+        USDData.data.data.BTT.quote &&
+        USDData.data.data.BTT.quote.USD &&
+        USDData.data.data.BTT.quote.USD.price
         let trxPriceDataBTC = BTCData.data && 
         BTCData.data.data.BTT && 
         BTCData.data.data.BTT.quote && 
         BTCData.data.data.BTT.quote.BTC && 
         BTCData.data.data.BTT.quote.BTC.price;
         console.log(trxPriceDataUSD,'')
-        let priceUSD = ((parseFloat(trxPriceDataUSD.toFixed(10)))*1000).toFixed(3);
+        let priceUSD = ((parseFloat(trxPriceDataUSD))*1000).toFixed(3);
         let  x = new BigNumber(trxPriceDataBTC);
         let priceBTC = x.multipliedBy(1000).decimalPlaces(5).toNumber();
 
