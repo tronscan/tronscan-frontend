e3ee768d6e2c4fe625786a562e02990a3672c11e
Harry <431027103@qq.com>
2020-03-19 10:53:58 +0800

    Build shasta test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52f0c6283826280ef3a4b6dbdf4824b4.js"
+  "/precache-manifest.aed7302cf1e4e931834b92fa07f72421.js"
 );
 
 self.addEventListener('message', (event) => {
