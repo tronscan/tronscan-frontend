5cb8f50227f8b908895b35b01dbd071ad31fc24c
yq <11@163.com>
2020-03-25 14:58:54 +0800

    change api

--- src/constants.js
+++ src/constants.js
@@ -72,7 +72,7 @@ export const PUBLIC_URL = process.env.PUBLIC_URL || window.location.origin;
 //test pro
 //export const API_URL_SUNNET = "http://52.15.68.74:10001";
 
-export const API_URL_SUNNET = "https://niledappapi.tronscan.org ";
+export const API_URL_SUNNET = "https://niledappapi.tronscan.org";
 // export const API_URL_MAINNET = 'https://apilist.tronscan.org';
 export const API_URL = IS_SUNNET ? API_URL_SUNNET : process.env.API_URL;
 export const CONTRACT_MAINNET_API_URL = process.env.API_URL;
