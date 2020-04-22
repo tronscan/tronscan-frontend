baae2242d51620ea4cdf2d306286431da38477b9
Richard <codewellliu@gmail.com>
2020-04-22 15:14:58 +0800

    env change

--- config/env.js
+++ config/env.js
@@ -85,7 +85,6 @@ function getClientEnvironment(publicUrl) {
         // API_URL: process.env.API_URL || `http://18.190.104.70:9000`,
         API_URL: process.env.API_URL ||  `https://tronexapi.tronscan.org`,
         // API_URL: process.env.API_URL ||  `https://nileapi.tronscan.org`,
-
         NET: process.env.NET || `mainnet`,
         DESKTOP: process.env.DESKTOP || `false`
       }
