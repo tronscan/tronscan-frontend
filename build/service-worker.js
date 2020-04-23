2bba42054297e3ea31978c4dcfbefb80961625bd
yq <11@163.com>
2020-04-23 17:31:56 +0800

    build fro test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://static.tronscan.io:5577/precache-manifest.20e9cf087da3294dc1a1b4d45c119f69.js"
+  "https://static.tronscan.io:5577/precache-manifest.85790c2685a908f51c3ed990610118e6.js"
 );
 
 self.addEventListener('message', (event) => {
