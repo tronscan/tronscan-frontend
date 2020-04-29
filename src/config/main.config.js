6c302273dfa249364f3b166cce2180f6798e3d71
Harry <431027103@qq.com>
2020-04-29 14:57:28 +0800

    Set online config

--- src/config/main.config.js
+++ src/config/main.config.js
@@ -1,12 +1,10 @@
 
 let config = {
     curEnv:'production',
-    // networkUrl : `https://api.trongrid.io`,
-    networkUrl : `https://api.shasta.trongrid.io`,
+    networkUrl : `https://api.trongrid.io`,
     api:{
         mutiSign:{
-            // apiPostMutiSignedTansaction:'https://list.tronlink.org/api/wallet/multi/transaction'
-            apiPostMutiSignedTansaction:'https://testlist.tronlink.org/api/wallet/multi/transaction'
+            apiPostMutiSignedTansaction:'https://list.tronlink.org/api/wallet/multi/transaction'
         }
     }
 }
