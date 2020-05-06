012a060578530549edc8faf4455cc125173b7bad
tronfamily <55282514+tronfamily@users.noreply.github.com>
2020-05-06 21:53:15 +0200

    Create de.js

---
+++ src/translations/versionLang/lang20Q2/de.js
@@ -0,0 +1,33 @@
+/*
+    ##################################################################################
+    #                                                                                #
+    #  $$$$$$$$\                                                                     #
+    #  \__$$  __|                                                                    #
+    #     $$ | $$$$$$\   $$$$$$\  $$$$$$$\   $$$$$$$\  $$$$$$$\$$$$$$\  $$$$$$$\     #
+    #     $$ |$$  __$$\ $$  __$$\ $$  __$$\ $$  _____|$$  _____\____$$\ $$  __$$\    #
+    #     $$ |$$ |  \__|$$ /  $$ |$$ |  $$ |\$$$$$$\  $$ /     $$$$$$$ |$$ |  $$ |   #
+    #     $$ |$$ |      $$ |  $$ |$$ |  $$ | \____$$\ $$ |    $$  __$$ |$$ |  $$ |   #
+    #     $$ |$$ |      \$$$$$$  |$$ |  $$ |$$$$$$$  |\$$$$$$$\$$$$$$$ |$$ |  $$ |   #
+    #     \__|\__|       \______/ \__|  \__|\_______/  \_______\_______|\__|  \__    #
+    #                                                                                #
+    ##################################################################################
+    #                                                                                #
+    #         Template for translating the tronscan.org website                      #
+    #         Version: 2020 Q2                                                       #
+    #         Update Date: 04.2019                                                   #
+    #         Language: Deutsch                                                      #
+    #         Status: Approved Version                                               #
+    #         Number of checks: 0                                                    #
+    #                                                                                #
+    ##################################################################################
+*/
+export const lang20Q2 = {
+    //well 20200414
+    index_page_nav_more_support:'Unterstütung&Hilfe',
+    index_page_nav_more_guide:"HANDBUCH FÜR ANFÄNGER",
+    index_page_nav_more_service:'SERVICE',
+    index_page_price_loading:'Lade',
+    index_page_price_time_out:'Zeit abgelaufen',
+    
+    open_tron_app_on_verify_address: "Bitte bestätigen Sie die Anmeldung bei TRON APP",
+}
